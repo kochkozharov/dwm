86e7a8e57301093d71ee022da006ac8aef15fb07
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2023-04-02 16:14:12 +0300

    switched to light util

--- config.def.h
+++ config.def.h
@@ -67,8 +67,8 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *brightnessup[] = { "xbacklight", "-inc", "5", NULL };
-static const char *brightnessdown[] = { "xbacklight", "-dec", "5", NULL };
+static const char *brightnessup[] = { "light", "-A", "5", NULL };
+static const char *brightnessdown[] = { "light", "-U", "5", NULL };
 static const char *flameshot[] = {"flameshot", "gui", NULL};
 
 static const Key keys[] = {
